db66e0b8ae830a2cc5aeebe1c81e027148a664d3
sangvo <sangvo111@gmail.com>
2022-09-15 15:39:16 +0700

    Fix: nvim tree with nvim 0.7

--- config/nvim/lua/module-nvim-tree.lua
+++ config/nvim/lua/module-nvim-tree.lua
@@ -1,6 +1,7 @@
 vim.g.loaded = 1
 vim.g.loaded_netrwPlugin = 1
 
+
 require("nvim-tree").setup({
   sort_by = "case_sensitive",
   view = {
@@ -17,4 +18,25 @@ require("nvim-tree").setup({
   filters = {
     dotfiles = true,
   },
+  update_focused_file = {
+    enable = true,
+    update_root = false,
+    ignore_list = {},
+  },
 })
+
+-- Mappings for nvimtree
+vim.api.nvim_set_keymap(
+    "n",
+    "<C-b>",
+    ":NvimTreeToggle<CR>",
+    {
+        noremap = true,
+        silent = true
+    }
+)
+
+-- Change color folder
+vim.cmd[[hi NvimTreeFolderName guifg = '#6699cc']]
+
+-- See more mappings: :help nvim-tree.view.mappings
