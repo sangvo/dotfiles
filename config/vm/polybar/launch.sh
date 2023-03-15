7b11f10861ced91c95ed71006cc26df87f1ba412
sangvo <sangvo111@gmail.com>
2023-03-15 17:20:23 +0700

    VM: update

--- config/vm/polybar/launch.sh
+++ config/vm/polybar/launch.sh
@@ -5,13 +5,16 @@ killall -q polybar
 # If all your bars have ipc enabled, you can also use
 # polybar-msg cmd quit
 
-# Launch bar1 and bar2
-if type "xrandr"; then
-  for m in $(xrandr --query | grep " connected" | cut -d" " -f1); do
-    MONITOR=$m polybar --reload mybar &
-  done
-else
-  polybar --reload mybar &
-fi
+PRIMARY=$(xrandr --query | grep " connected" | grep "primary" | cut -d" " -f1)
+OTHERS=$(xrandr --query | grep " connected" | grep -v "primary" | cut -d" " -f1)
+
+# Launch on primary monitor
+MONITOR=$PRIMARY polybar --reload main &
+sleep 1
+
+# Launch on all other monitors
+for m in $OTHERS; do
+ MONITOR=$m polybar --reload rightbar &
+done
 
 echo "Bars launched..."
