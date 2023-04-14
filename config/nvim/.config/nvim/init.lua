93bd8cbde659960b3424b1f431fb5b775609ab51
sangvo <sangvo111@gmail.com>
2023-04-14 16:15:45 +0700

    Disable netrw vim built in

--- config/nvim/.config/nvim/init.lua
+++ config/nvim/.config/nvim/init.lua
@@ -15,6 +15,10 @@ vim.opt.rtp:prepend(lazypath)
 vim.g.mapleader = " "
 vim.g.maplocalleader = " "
 
+-- disable netrw at the very start of your init.lua (strongly advised)
+vim.g.loaded_netrw = 1
+vim.g.loaded_netrwPlugin = 1
+
 require("lazy").setup("sang.plugins", {
   defaults = { version = false },
   performance = {
