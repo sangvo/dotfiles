dc3b37579ea7e34c7d6c0891c121bc8cf22926a4
sangvo <sangvo111@gmail.com>
2024-06-13 21:49:45 +0700

    update wezterm

--- config/wezterm/.config/wezterm/config/appearance.lua
+++ config/wezterm/.config/wezterm/config/appearance.lua
@@ -1,23 +1,9 @@
-local wezterm = require 'wezterm'
-
 return {
-  color_scheme = 'rose-pine-moon',
+	color_scheme = "tokyonight_night",
 
-  window_decorations = 'RESIZE',
-  use_fancy_tab_bar = false,
+	window_decorations = "RESIZE",
+	use_fancy_tab_bar = false,
 
-  show_tab_index_in_tab_bar = false,
-  switch_to_last_active_tab_when_closing_tab = true,
-
-  colors = {
-    tab_bar = {
-      background = '#393552',
-      new_tab = {
-        bg_color = '#393552',
-	      fg_color = '#e0def4'
-      }
-    },
-    selection_bg = '#fffacd',
-    selection_fg = 'black',
-  },
+	show_tab_index_in_tab_bar = false,
+	switch_to_last_active_tab_when_closing_tab = true,
 }
--- config/wezterm/.config/wezterm/events/right-status.lua
+++ config/wezterm/.config/wezterm/events/right-status.lua
@@ -1,20 +1,14 @@
-local wezterm = require 'wezterm'
+local wezterm = require("wezterm")
 
 local M = {}
 
 M.setup = function()
-  wezterm.on("update-right-status", function(window, pane)
-
-    local status = wezterm.format({
-	      {Foreground={Color="#5e8052"}},
-      	{Background={Color="#3c3836"}},
-      	{Text=""},
-      	{Foreground={Color="#1d2021"}},
-      	{Background={Color="#5e8052"}},
-      	{Text="Full Stuck Developer"},
-    })
-    window:set_right_status(status)
-  end)
+	wezterm.on("update-right-status", function(window, _pane)
+		local status = wezterm.format({
+			{ Text = "💛 ⭐ 💜" },
+		})
+		window:set_right_status(status)
+	end)
 end
 
 return M
