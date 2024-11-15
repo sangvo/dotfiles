42efa32d60dd0b4d7b643de535e3e97f6f2d55c0
sangvo <sangvo111@gmail.com>
2024-11-15 16:11:56 +0700

    Update right status

--- config/wezterm/.config/wezterm/events/right-status.lua
+++ config/wezterm/.config/wezterm/events/right-status.lua
@@ -1,20 +1,26 @@
-local wezterm = require 'wezterm'
+local wezterm = require("wezterm")
 
 local M = {}
 
 M.setup = function()
-  wezterm.on("update-right-status", function(window, pane)
+	wezterm.on("update-right-status", function(window, pane)
+		local status = wezterm.format({
+			{ Foreground = { Color = "#797593" } },
+			{ Background = { Color = "#3c3836" } },
+			{ Text = "" },
+			{ Foreground = { Color = "#dfdad9" } },
+			{ Background = { Color = "#797593" } },
+			{ Text = "     󰮯 " },
 
-    local status = wezterm.format({
-	      {Foreground={Color="#5e8052"}},
-      	{Background={Color="#3c3836"}},
-      	{Text=""},
-      	{Foreground={Color="#1d2021"}},
-      	{Background={Color="#5e8052"}},
-      	{Text="Full Stuck Developer"},
-    })
-    window:set_right_status(status)
-  end)
+			{ Foreground = { Color = "#d7827e" } },
+			{ Background = { Color = "#797593" } },
+			{ Text = "" },
+			{ Foreground = { Color = "#dfdad9" } },
+			{ Background = { Color = "#d7827e" } },
+			{ Text = "  SANG-VO   " },
+		})
+		window:set_right_status(status)
+	end)
 end
 
 return M
